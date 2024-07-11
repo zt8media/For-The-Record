2e9ea7103382be9c2e6ffac80ca0d13979f9db26
zt8media <zt8media@gmail.com>
2024-07-11 15:42:26 -0400

    server/file set up

---
+++ Server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,"public")))
+app.use(cors())
+app.use(express.json())
+
+const port = 5173
+
+const db = mysql.createConnection ({
+    host:"localhost",
+    user:"root",
+    passord:"password",
+    database:"Products"
+})
+
+app.listen(port,()=>{
+    console.log('listening')
+})
+
